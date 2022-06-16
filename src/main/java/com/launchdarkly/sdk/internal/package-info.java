a4134195c982a8637c093cdedd4b8d60ac856993
Eli Bishop <eli@launchdarkly.com>
2022-06-15 18:24:50 -0700

    javadoc

---
+++ src/main/java/com/launchdarkly/sdk/internal/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * General-purpose helper types and methods for use in LaunchDarkly SDK code.
+ * <p>
+ * All symbols in this package are for internal SDK use only. They are subject to change
+ * without notice, and any direct reference to them in application code is unsupported.
+ * They have public scope only because they need to be available to LaunchDarkly SDK
+ * code in other packages.
+ */
+package com.launchdarkly.sdk.internal;
