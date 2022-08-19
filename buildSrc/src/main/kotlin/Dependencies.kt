49e8baec502cc74425ff23f3ddc88de93b0767b9
Eli Bishop <eli@launchdarkly.com>
2022-08-18 18:37:18 -0700

    fix dependencies: don't want Guava or SLF4J

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,11 +4,9 @@
 
 object Versions {
     const val gson = "2.8.9"
-    const val guava = "30.1-jre"
     const val launchdarklyJavaSdkCommon = "2.0.0-SNAPSHOT"
     const val launchdarklyLogging = "1.1.1"
     const val okhttp = "4.9.1"
-    const val slf4j = "1.7.21"
     const val testHelpers = "1.2.0"
 }
 
@@ -22,16 +20,15 @@ object Libs {
         // We would put anything here that we want to go into the Gradle "implementation"
         // configuration, if and only if we want those things to show up in pom.xml.
         "com.google.code.gson:gson:${Versions.gson}",
-        "com.google.guava:guava:${Versions.guava}",
         "com.launchdarkly:launchdarkly-java-sdk-common:${Versions.launchdarklyJavaSdkCommon}",
         "com.launchdarkly:launchdarkly-logging:${Versions.launchdarklyLogging}",
-        "com.squareup.okhttp3:okhttp:${Versions.okhttp}",
-        "org.slf4j:slf4j-api:${Versions.slf4j}"
+        "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
     )
 
     val javaTestImplementation = listOf(
         "junit:junit:4.12",
-        "org.hamcrest:hamcrest-library:1.3"
+        "org.hamcrest:hamcrest-library:1.3",
+        "com.google.guava:guava:30.1-jre"
         
         // "com.launchdarkly:test-helpers:${Versions.testHelpers}"
         // test-helpers is special-cased in build.gradle.kts and build-android.gradle
